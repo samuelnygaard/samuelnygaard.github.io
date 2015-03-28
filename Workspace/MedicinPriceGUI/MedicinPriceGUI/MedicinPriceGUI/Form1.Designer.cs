9bb9528ff63a9c70db107d2ea0b4a6e4ca4356f5
Samuel Nygaard <snpe12@student.aau.dk>
2015-03-28 19:25:16 +0100

    Add GUI for MedPrice

---
+++ Workspace/MedicinPriceGUI/MedicinPriceGUI/MedicinPriceGUI/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace MedicinPriceGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.newestZip = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // newestZip
+            // 
+            this.newestZip.Location = new System.Drawing.Point(216, 34);
+            this.newestZip.Name = "newestZip";
+            this.newestZip.Size = new System.Drawing.Size(56, 23);
+            this.newestZip.TabIndex = 0;
+            this.newestZip.Tag = "newestZip";
+            this.newestZip.Text = "Browse";
+            this.newestZip.UseVisualStyleBackColor = true;
+            this.newestZip.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 37);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(197, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "\r\n";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Newest ZIP file:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.newestZip);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button newestZip;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+
+
+
+    }
+}
+
---
+++ Workspace/MedicinPriceGUI/MedicinPriceGUI/MedicinPriceGUI/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MedicinPriceGUI
+{
+    public partial class Form1 : Form
+    {
+        static string newZipFilePath = "C:\\";
+
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.Text = newZipFilePath;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog newZipFileDialog = new OpenFileDialog();
+            var d = newZipFileDialog;
+            d.InitialDirectory = "C:\\";
+            d.Filter = "ZIP files (*.zip)|*.zip|All files (*.*)|*.*";
+            d.FilterIndex = 1;
+            d.RestoreDirectory = true;
+
+            if (d.ShowDialog() == DialogResult.OK)
+            {
+                newZipFilePath = d.FileName;
+                textBox1.Text = newZipFilePath;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            newZipFilePath = Text;
+        }
+    }
+}
