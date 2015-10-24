58e196b5aa38ad159ec8c3936c442a1d7364b106
Samuel Nygaard <snpe12@student.aau.dk>
2015-10-24 16:47:54 +0200

    Added project themplate

--- assets/js/util.js
+++ assets/js/util.js
@@ -20,6 +20,51 @@
             }
         });
     });
+	
+	/********
+		Skills circels
+	********/
+	$(window).load(function() {
+		doughnutWidget.options = {
+			container: $('#skill-chart'),
+			width: 100,
+			height: 100,
+			class: 'myClass',
+			cutout: 50
+		};
+
+		doughnutWidget.render(data());
+
+		setInterval(init, 2000);
+	});
+
+	function init() {
+		doughnutWidget.render(data());
+	}
+
+	function data() {
+		var data = {
+			pending: {
+				val: Math.round(Math.random() * 100),
+				color: '#57B4F2',
+				click: function(e) {
+					console.log('hi');
+				}
+			},
+			delivered: {
+				val: Math.round(Math.random() * 100),
+				color: '#6DED5C'
+			},
+			delayed: {
+				val: Math.round(Math.random() * 100),
+				color: '#E63329',
+				link: 'http://www.google.com'
+			}
+		};
+
+		return data;
+	}
+	
 
     /********
     	Contact formula
---
+++ assets/js/util_grid.js
@@ -0,0 +1,195 @@
+
+    $(function(){
+        $("#elastic-grid").elastic_grid({
+            'filterEffect': 'popup', // moveup, scaleup, fallperspective, fly, flip, helix , popup
+            'hoverDirection': true,
+            'hoverDelay': 0,
+            'hoverInverse': false,
+            'expandingSpeed': 500,
+            'expandingHeight': 500,
+            'items' :
+            [
+                {
+                    'title'         : 'Lorem ipsum',
+                    'description'   : 'Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum ',
+                    'thumbnail'     : ['images/1.png', 'images/2.png', 'images/3.png', 'images/1.png', 'images/1.png'],
+                    'large'         : ['images/1.png', 'images/2.png', 'images/3.png', 'images/10.png', 'images/1.png'],
+                    'button_list'   :
+                    [
+                        { 'title':'Demo', 'url' : 'http://www.jqueryscript.net' },
+                        { 'title':'Download', 'url':'http://www.jqueryscript.net'}
+                    ],
+                    'tags'          : ['University Projects']
+                },
+                {
+                    'title'         : 'Lorem ipsum ',
+                    'description'   : 'Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum ',
+                    'thumbnail'     : ['images/4.png', 'images/5.png', 'images/6.png', 'images/6.png'],
+                    'large'         : ['images/4.png', 'images/5.png', 'images/6.png', 'images/6.png'],
+                    'button_list'   :
+                    [
+                        { 'title':'Demo', 'url' : 'http://www.jqueryscript.net' },
+                        { 'title':'Download', 'url':'http://www.jqueryscript.net'}
+                    ],
+                    'tags'          : ['Software Projects']
+                },
+                {
+                    'title'         : 'Lorem ipsum ',
+                    'description'   : 'Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum ',
+                    'thumbnail'     : ['images/3.png','images/5.png', 'images/4.png', 'images/1.png'],
+                    'large'         : ['images/5.png','images/5.png', 'images/4.png', 'images/10.png'],
+                    'button_list'   :
+                    [
+                        { 'title':'Demo', 'url' : 'http://www.jqueryscript.net' },
+                        { 'title':'Download', 'url':'http://www.jqueryscript.net'}
+                    ],
+                    'tags'          : ['University Projects', 'Software Projects']
+                },
+                {
+                    'title'         : 'Lorem ipsum ',
+                    'description'   : 'Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum ',
+                    'thumbnail'     : ['images/2.png', 'images/2.png', 'images/3.png', 'images/3.png', 'images/4.png'],
+                    'large'         : ['images/2.png', 'images/3.png', 'images/4.png', 'images/5.png', 'images/6.png'],
+                    'button_list'   :
+                    [
+                        { 'title':'Demo', 'url' : 'http://www.jqueryscript.net' },
+                        { 'title':'Download', 'url':'http://www.jqueryscript.net'}
+                    ],
+                    'tags'          : ['University Projects']
+                },
+                {
+                    'title'         : 'Lorem ipsum ',
+                    'description'   : 'Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum ',
+                    'thumbnail'     : ['images/4.png', 'images/5.png', 'images/4.png', 'images/1.png'],
+                    'large'         : ['images/6.png', 'images/5.png', 'images/4.png', 'images/1.png'],
+                    'button_list'   :
+                    [
+                        { 'title':'Demo', 'url' : 'http://www.jqueryscript.net' },
+                        { 'title':'Download', 'url':'http://www.jqueryscript.net'}
+                    ],
+                    'tags'          : ['Software Projects']
+                },
+                {
+                    'title'         : 'Lorem ipsum ',
+                    'description'   : 'Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum ',
+                    'thumbnail'     : ['images/2.png','images/3.png', 'images/1.png', 'images/1.png'],
+                    'large'         : ['images/3.png','images/5.png', 'images/1.png', 'images/10.png'],
+                    'button_list'   :
+                    [
+                        { 'title':'Demo', 'url' : 'http://www.jqueryscript.net' },
+                        { 'title':'Download', 'url':'http://www.jqueryscript.net'}
+                    ],
+                    'tags'          : ['Life Projects']
+                },
+                {
+                    'title'         : 'Lorem ipsum ',
+                    'description'   : 'Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum ',
+                    'thumbnail'     : ['images/6.png','images/5.png', 'images/4.png', 'images/1.png'],
+                    'large'         : ['images/6.png','images/5.png', 'images/4.png', 'images/10.png'],
+                    'button_list'   :
+                    [
+                        { 'title':'Demo', 'url' : 'http://www.jqueryscript.net' },
+                        { 'title':'Download', 'url':'http://www.jqueryscript.net'}
+                    ],
+                    'tags'          : ['University Projects']
+                },
+                {
+                    'title'         : 'Lorem ipsum',
+                    'description'   : 'Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum ',
+                    'thumbnail'     : ['images/4.png', 'images/2.png', 'images/3.png', 'images/3.png', 'images/4.png'],
+                    'large'         : ['images/6.png', 'images/3.png', 'images/4.png', 'images/5.png', 'images/6.png'],
+                    'button_list'   :
+                    [
+                        { 'title':'Demo', 'url' : 'http://www.jqueryscript.net' },
+                        { 'title':'Download', 'url':'http://www.jqueryscript.net'}
+                    ],
+                    'tags'          : ['Life Projects']
+                },
+                {
+                    'title'         : 'Lorem ipsum ',
+                    'description'   : 'Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum ',
+                    'thumbnail'     : ['images/5.png', 'images/5.png', 'images/4.png', 'images/1.png'],
+                    'large'         : ['images/5.png', 'images/5.png', 'images/4.png', 'images/1.png'],
+                    'button_list'   :
+                    [
+                        { 'title':'Demo', 'url' : 'http://www.jqueryscript.net' },
+                        { 'title':'Download', 'url':'http://www.jqueryscript.net'}
+                    ],
+                    'tags'          : ['Software Projects']
+                },
+                {
+                    'title'         : 'Lorem ipsum ',
+                    'description'   : 'Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum ',
+                    'thumbnail'     : ['images/1.png','images/3.png', 'images/1.png', 'images/1.png'],
+                    'large'         : ['images/1.png','images/5.png', 'images/1.png', 'images/10.png'],
+                    'button_list'   :
+                    [
+                        { 'title':'Demo', 'url' : 'http://www.jqueryscript.net' },
+                        { 'title':'Download', 'url':'http://www.jqueryscript.net'}
+                    ],
+                    'tags'          : ['University Projects']
+                },
+                {
+                    'title'         : 'Lorem ipsum ',
+                    'description'   : 'Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum ',
+                    'thumbnail'     : ['images/3.png','images/3.png', 'images/1.png', 'images/1.png'],
+                    'large'         : ['images/3.png','images/5.png', 'images/1.png', 'images/10.png'],
+                    'button_list'   :
+                    [
+                        { 'title':'Demo', 'url' : 'http://www.jqueryscript.net' },
+                        { 'title':'Download', 'url':'http://www.jqueryscript.net'}
+                    ],
+                    'tags'          : ['Life Projects']
+                },
+                {
+                    'title'         : 'Lorem ipsum ',
+                    'description'   : 'Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum ',
+                    'thumbnail'     : ['images/5.png','images/5.png', 'images/4.png', 'images/1.png'],
+                    'large'         : ['images/5.png','images/5.png', 'images/4.png', 'images/10.png'],
+                    'button_list'   :
+                    [
+                        { 'title':'Demo', 'url' : 'http://www.jqueryscript.net' },
+                        { 'title':'Download', 'url':'http://www.jqueryscript.net'}
+                    ],
+                    'tags'          : ['University Projects', 'Software Projects']
+                },
+                {
+                    'title'         : 'Lorem ipsum',
+                    'description'   : 'Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum ',
+                    'thumbnail'     : ['images/6.png', 'images/2.png', 'images/3.png', 'images/3.png', 'images/4.png'],
+                    'large'         : ['images/6.png', 'images/3.png', 'images/4.png', 'images/5.png', 'images/6.png'],
+                    'button_list'   :
+                    [
+                        { 'title':'Demo', 'url' : 'http://www.jqueryscript.net' },
+                        { 'title':'Download', 'url':'http://www.jqueryscript.net'}
+                    ],
+                    'tags'          : ['Life Projects']
+                },
+                {
+                    'title'         : 'Lorem ipsum ',
+                    'description'   : 'Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum ',
+                    'thumbnail'     : ['images/5.png', 'images/5.png', 'images/4.png', 'images/1.png'],
+                    'large'         : ['images/5.png', 'images/5.png', 'images/4.png', 'images/1.png'],
+                    'button_list'   :
+                    [
+                        { 'title':'Demo', 'url' : 'http://www.jqueryscript.net' },
+                        { 'title':'Download', 'url':'http://www.jqueryscript.net'}
+                    ],
+                    'tags'          : ['Software Projects']
+                },
+                {
+                    'title'         : 'Lorem ipsum ',
+                    'description'   : 'Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum ',
+                    'thumbnail'     : ['images/4.png','images/3.png', 'images/1.png', 'images/1.png'],
+                    'large'         : ['images/4.png','images/5.png', 'images/1.png', 'images/10.png'],
+                    'button_list'   :
+                    [
+                        { 'title':'Demo', 'url' : 'http://www.jqueryscript.net' },
+                        { 'title':'Download', 'url':'http://www.jqueryscript.net'}
+                    ],
+                    'tags'          : ['Life Projects', 'Software Projects']
+                }
+
+            ]
+        });
+    });
